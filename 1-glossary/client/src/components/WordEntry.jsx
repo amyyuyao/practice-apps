1da267d66c52c715e304cf7e114a8b71ae3e5edb
Yao Yu <amyyuyao.thu@gmail.com>
2022-05-13 00:40:20 -0400

    finish edit and submit buttons

--- 1-glossary/client/src/components/WordEntry.jsx
+++ 1-glossary/client/src/components/WordEntry.jsx
@@ -5,8 +5,8 @@ class WordEntry extends React.Component {
     super(props);
 
     this.state = {
-      word: props.word,
-      definition: props.definition,
+      word: props.entry.word,
+      definition: props.entry.definition,
       editable: 'false'
     };
 
@@ -24,39 +24,42 @@ class WordEntry extends React.Component {
 
   handleWordChange(e) {
     this.setState({
-      inputWord: e.target.value
+      word: e.target.value
     })
   }
 
   handleDefChange(e) {
     this.setState({
-      inputDef: e.target.value
+      definition: e.target.value
     })
   }
 
   handleEditSubmit(e) {
     alert('Are you sure to change the definition of the word?');
     this.props.onEdit(this.state.word, this.state.definition);
+    this.setState({
+      editable: "false"
+    })
   }
 
   render() {
     return (
-      <tbody>
+      <table>
         <tr>
         { this.state.editable === 'true'
-          ? (<div>
+          ? (<>
             <td><input type="text" onChange={this.handleWordChange} value={this.state.word}/></td>
             <td><input type="text" onChange={this.handleDefChange} value={this.state.definition}/></td>
             <td><button onClick={this.handleEditSubmit}>Submit change</button></td>
-            </div>) : (<div>
+            </>) : (<>
             <td>{this.state.word}</td>
             <td>{this.state.definition}</td>
             <td><button onClick={this.handleEdit}>Edit</button></td>
           {/* <td><button onClick={this.handleDelete}>Delete</button></td> */}
-          </div>)
+          </>)
         }
         </tr>
-      </tbody>
+      </table>
     )
   }
 }
